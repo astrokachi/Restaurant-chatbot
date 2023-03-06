4addf6e02b3a882c7c0197d10c031055acbee1c7
Kachi <astrokachi@gmail.com>
2023-03-06 19:02:00 +0100

    chore: initial setup

---
+++ app.ts
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+//create server
+const http = require("http");
+const server = http.createServer(app);
+//initialize io
+const io = require("socket.io")(server, { cors: { origin: "*" } });
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req: any, res: any) => {
+	res.send("index.html");
+});
+
+server.listen(3000, () => {
+	console.log("server is listening on port", 3000);
+});
+
+io.on("connection", (socket: any) => {
+	console.log("Someone connected!");
+});
