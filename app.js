6a230cca166134c8d4fb4143d7ddf744dcb6824f
Kachi <astrokachi@gmail.com>
2023-03-09 11:48:55 +0100

    feat: validate input on initial prop

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const app = express();
 //create server
@@ -14,6 +15,64 @@ app.get("/", (req, res) => {
 server.listen(3000, () => {
     console.log("server is listening on port", 3000);
 });
-io.on("connection", (socket) => {
-    console.log("Someone connected!");
+const orderHistory = [];
+let currentOrder = "";
+// do an api call to return random list of foods
+const foods = [
+    "Curry rice",
+    "Beans and bread",
+    "Mashed Potatoes",
+    "Ofada rice and assorted stew",
+    "Fetuccini",
+    "Bolognese",
+    "Spaghetti",
+    "Yamarita and egg sauce",
+    "Jollof rice",
+    "Swallow",
+];
+// const swallow = ["Eba", "Pounded yam", "Semo"];
+// const soups = ["Efo riro", "Ewedu", "Egusi", "Oha"];
+io.on("connect", (socket) => {
+    console.log("Someone connected!", socket.id);
+    socket.on("message", (message) => {
+        if (message == 1) {
+            socket.emit("place an order", foods);
+        }
+        else if (message == 99) {
+            if (currentOrder.length > 0) {
+                orderHistory.push(currentOrder);
+            }
+            else {
+                socket.emit("no order", "No order to place");
+            }
+        }
+        else if (message == 98) {
+            if (orderHistory.length > 0) {
+                socket.emit("all placed orders", orderHistory);
+            }
+            else {
+                socket.emit("no history", "You haven't made any order yet");
+            }
+        }
+        else if (message == 97) {
+            socket.emit("current order", currentOrder);
+        }
+        else if (message == 0) {
+            currentOrder = "";
+        }
+        else {
+            socket.emit("invalid input", "Please enter a valid input");
+            console.log("end");
+        }
+    });
+    socket.on("add order", (value) => {
+        const current = foods.filter((food, i) => value === i + 1)[0];
+        if (currentOrder.length > 0) {
+            currentOrder += `, ${current}`;
+        }
+        else {
+            currentOrder += current;
+        }
+        console.log(currentOrder);
+    });
 });
--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
+import { Socket } from "socket.io";
+
 const express = require("express");
 const app = express();
 //create server
 const http = require("http");
 const server = http.createServer(app);
 //initialize io
-const io = require("socket.io")(server, { cors: { origin: "*" } });
+const io: Socket = require("socket.io")(server, { cors: { origin: "*" } });
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
@@ -17,6 +19,60 @@ server.listen(3000, () => {
 	console.log("server is listening on port", 3000);
 });
 
-io.on("connection", (socket: any) => {
-	console.log("Someone connected!");
+const orderHistory: string[] = [];
+let currentOrder = "";
+// do an api call to return random list of foods
+const foods = [
+	"Curry rice",
+	"Beans and bread",
+	"Mashed Potatoes",
+	"Ofada rice and assorted stew",
+	"Fetuccini",
+	"Bolognese",
+	"Spaghetti",
+	"Yamarita and egg sauce",
+	"Jollof rice",
+	"Swallow",
+];
+// const swallow = ["Eba", "Pounded yam", "Semo"];
+// const soups = ["Efo riro", "Ewedu", "Egusi", "Oha"];
+io.on("connect", (socket) => {
+	console.log("Someone connected!", socket.id);
+
+	socket.on("message", (message: number) => {
+		if (message == 1) {
+			socket.emit("place an order", foods);
+		} else if (message == 99) {
+			if (currentOrder.length > 0) {
+				orderHistory.push(currentOrder);
+			} else {
+				socket.emit("no order", "No order to place");
+			}
+		} else if (message == 98) {
+			if (orderHistory.length > 0) {
+				socket.emit("all placed orders", orderHistory);
+			} else {
+				socket.emit("no history", "You haven't made any order yet");
+			}
+		} else if (message == 97) {
+			socket.emit("current order", currentOrder);
+		} else if (message == 0) {
+			currentOrder = "";
+		} else {
+			socket.emit("invalid input", "Please enter a valid input");
+			console.log("end");
+		}
+	});
+
+
+	socket.on("add order", (value: number) => {
+		const current = foods.filter((food, i) => value === i + 1)[0];
+		if (currentOrder.length > 0) {
+			currentOrder += `, ${current}`;
+		} else {
+			currentOrder += current;
+		}
+		
+		console.log(currentOrder);
+	});
 });
